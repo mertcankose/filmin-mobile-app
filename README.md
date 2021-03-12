8755992effe5c2fdea97f45e4b7047a4b8fedf2d
mertcankose <mertcankose97@outlook.com>
2021-03-12 20:16:09 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Filminn-Landing-Page
+
+## 🧐 About The Project
+Landing Page
+
+![Product Name Screen Shot][product-screenshot1]
+
+<!-- MARKDOWN & IMAGES -->
+[product-screenshot1]: src/img/screenshot.png
---
+++ src/js/index.js
@@ -0,0 +1,35 @@
+const headerSwitch = document.querySelector(".header-switch");
+const headerNavigation = document.querySelector(".header-nav");
+const headerOverlay = document.querySelector(".header-overlay");
+const body = document.body;
+
+headerSwitch.addEventListener("click", (e) => {
+  e.stopPropagation();
+  if(headerNavigation.classList.contains("nav-show")){
+    body.classList.remove("noscroll");
+    headerNavigation.classList.remove("nav-show");
+    headerSwitch.classList.remove("open");
+    headerOverlay.classList.remove("overlay-show");
+    headerOverlay.classList.add("fade-out");
+    headerOverlay.classList.remove("fade-in");
+  }
+  else{
+    body.classList.add("noscroll");
+    headerNavigation.classList.add("nav-show");
+    headerSwitch.classList.add("open");
+    headerOverlay.classList.add("overlay-show");
+    headerOverlay.classList.add("fade-in");
+    headerOverlay.classList.remove("fade-out");
+  }
+});
+
+document.documentElement.addEventListener("click", () => {
+  console.log("asdasf");
+  body.classList.remove("noscroll");
+  headerSwitch.classList.remove("open");
+  headerNavigation.classList.remove("nav-show");
+  headerOverlay.classList.remove("overlay-show");
+  headerOverlay.classList.add("fade-out");
+  headerOverlay.classList.remove("fade-in");
+});
+
